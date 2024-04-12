4939ad683f3f3a8874a5cb00de3749b16bcd1a6d
FHC <38954889+FHCProm@users.noreply.github.com>
2024-04-13 00:52:52 +0800

    removed vue devtool due to incompatibility

--- src/background.js
+++ src/background.js
@@ -2,7 +2,7 @@
 
 import { app, protocol, BrowserWindow, Menu } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
-import installExtension, { VUEJS3_DEVTOOLS } from "electron-devtools-installer";
+
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Scheme must be registered before the app is ready
@@ -69,12 +69,6 @@ app.on("activate", () => {
 // Some APIs can only be used after this event occurs.
 app.on("ready", async () => {
   if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS3_DEVTOOLS);
-    } catch (e) {
-      console.error("Vue Devtools failed to install:", e.toString());
-    }
   }
   createWindow();
 });
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import devtools from "@vue/devtools";
-import { createPinia } from 'pinia'
+// import devtools from "@vue/devtools";
+import { createPinia } from "pinia";
 
 if (process.env.NODE_ENV === "development") {
-  devtools.connect(/* host, port */);
+  console.log("yeah on development now");
+  // devtools.connect("http://localhost");
 }
 
 createApp(App).use(createPinia()).mount("#app");
